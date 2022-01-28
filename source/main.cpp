7f86ec50129325935344fa007c8bfebf408b3a23
Bilwin <bilwin780@gmail.com>
2022-01-28 11:26:21 +0900

    :fire: cleanup

--- source/main.cpp
+++ source/main.cpp
@@ -2,51 +2,6 @@
 #include "std.hpp"
 GarrysMod::Lua::ILuaInterface* JellyFish::ILuaServer;
 
-std::vector<std::string> split(const std::string& str, const std::string& delim) {
-	std::vector<std::string> keys;
-	size_t prev = 0, pos = 0;
-	do {
-		pos = str.find(delim, prev);
-		if (pos == std::string::npos)
-			pos = str.length();
-
-		std::string key = str.substr(prev, pos - prev);
-		if (!key.empty())
-			keys.push_back(key);
-		prev = pos + delim.length();
-
-	} while (pos < str.length() && prev < str.length());
-
-	return keys;
-}
-
-std::string exec(const char* cmd){
-	char buffer[128];
-	std::string result = "";
-	FILE* pipe = _popen(cmd, "r");
-	if (!pipe) throw std::runtime_error("popen() failed!");
-	try {
-		while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-			result += buffer;
-		}
-	}
-	catch (...) {
-		_pclose(pipe);
-		throw;
-	}
-	_pclose(pipe);
-	return result;
-}
-
-std::string replace_all(std::string subject, const std::string& search, const std::string& replace) {
-	size_t pos = 0;
-	while ((pos = subject.find(search, pos)) != std::string::npos) {
-		subject.replace(pos, search.length(), replace);
-		pos += replace.length();
-	}
-	return subject;
-}
-
 LUA_FUNCTION_STATIC(version)
 {
 	LUA->PushString(_MODULE_VERSION_);
