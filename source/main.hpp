bb4f4917e9d736de3e5df5a7f5f18fbf1417b196
Bilwin <bilwin780@gmail.com>
2022-01-28 11:14:10 +0900

    :wrench: Patch 04

--- source/main.hpp
+++ source/main.hpp
@@ -16,6 +16,10 @@
 #include <cstdlib>
 #include <string>
 #include <filesystem>
+#include <vector>
+#include <Wininet.h>
+#include <Windows.h>
+#pragma comment(lib, "wininet.lib")
 
 #define LUA_PUSHTYPEONCE(type, data) \
 LUA->Push##type(data); \
