010f794300bdf563d59e7d9772faaa470d49bce6
Bilwin <bilwin780@gmail.com>
2022-01-28 10:03:38 +0900

    :wrench: Patch-01

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,15 @@
 PROJECT_GENERATOR_VERSION = 3
 include("third-party/garrysmod_common")
 
-CreateWorkspace { name = "rnlib" }
+CreateWorkspace { name = "jellyfish" }
 	CreateProject { serverside = false }
 		IncludeHelpersExtended()
 		IncludeSDKTier0()
 		IncludeSDKTier1()
 		IncludeSDKCommon()
 		IncludeDetouring()
+		IncludeLuaShared()
+		IncludeScanning()
+		IncludeSteamAPI()
 
-		includedirs  "third-party/"
-		files {"source/*.cpp", "source/*.hpp", "third-party/**.*"}
+		files {"source/*.cpp", "source/*.hpp"}
--- source/main.hpp
+++ source/main.hpp
@@ -1,34 +1,31 @@
-#ifndef RNLIB_MAIN_H
-#define RNLIB_MAIN_H
+#ifndef JELLYFISH_MAIN_H
+#define JELLYFISH_MAIN_H
 
 #ifdef _WIN32
 	#pragma once
 #endif
 
-#define _MODULE_VERSION_ "0.1"
+#define _MODULE_VERSION_ "0.1.1"
 
 #include <GarrysMod/Lua/Interface.h>
 #include <GarrysMod/Lua/LuaInterface.h>
 #include <GarrysMod/FunctionPointers.hpp>
 #include <GarrysMod/InterfacePointers.hpp>
-#include <discord_rpc.h>
-#include <stdio.h>
-#include <string.h>
-#include <utility>
+
+#include <detouring/hook.hpp>
+#include <player.h>
+#include <string>
+
+#define LUA_PUSHTYPEONCE(type, data) \
+LUA->Push##type(data); \
+return 1;
 
 #define PUSHFUNC(name) \
 LUA->PushCFunction(name); \
 LUA->SetField(-2, #name);
 
-namespace rnlib {
+namespace JellyFish {
 	extern GarrysMod::Lua::ILuaInterface* ILuaServer;
-
-	HandleDiscordReady(const DiscordUser *connectedUser)
-	HandleDiscordDisconnected(int errcode, const char *message)
-	HandleDiscordError(int errcode, const char *message)
-	HandleDiscordJoin(const char *secret)
-	HandleDiscordSpectate(const char *secret)
-	HandleDiscordJoinRequest(const DiscordUser *request)
 }
 
 #endif
