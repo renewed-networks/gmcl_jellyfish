e79c0b12c4edd19ff55ee55b40913e1bfcbdd703
Bilwin <bilwin780@gmail.com>
2022-03-29 22:08:06 +0900

    :package: project fix

--- cmake/cpp/main.cpp
+++ cmake/cpp/main.cpp
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <utility>
 
+#define _MODULE_VERSION_ "1.0"
+
 DiscordUser cbConUser;
 std::pair<int, char> cbDisconnected;
 std::pair<int, char> cbError;
@@ -179,6 +181,12 @@ LUA_FUNCTION(UpdateDiscordStatus) {
     return 0;
 }
 
+LUA_FUNCTION_STATIC(jlRPCv)
+{
+	LUA->PushString(_MODULE_VERSION_);
+	return 1;
+}
+
 GMOD_MODULE_OPEN() {
     // Create the functions
     LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
@@ -199,6 +207,11 @@ GMOD_MODULE_OPEN() {
     LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
     LUA->PushCFunction(UpdateDiscordStatus);
     LUA->SetField(-2, "DiscordUpdateRPC");
+    LUA->Pop();
+
+    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+    LUA->PushCFunction(jlRPCv);
+    LUA->SetField(-2, "jellyfishRPCVersion");
     LUA->Pop();
 
     return 0;
